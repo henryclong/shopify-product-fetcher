f8d4fb0ccc2003cb7f3ab56a2182adc870f009bc
Henry Long <henry.cassetta.long@gmail.com>
2021-08-03 09:04:42 -0400

    Build fetchWithCrawlDelay function to respect crawl delay

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,31 @@ const fetch = require('node-fetch');
 
 const ROBOTS_PATH = '/robots.txt';
 
+const crawlDelay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getCrawlDelay = (delay, lastVisitTimestamp) => {
+    const currentTimestamp = Date.now();
+    let delay = (lastVisitTimestamp + delay) - currentTimestamp;
+    return delay > 0 ? delay : 0;
+}
+
+const fetchWithCrawlDelay = async (url, delay, lastVisitTimestamp) => {
+    const delayTime = getCrawlDelay(delay, lastVisitTimestamp);
+    const response = await crawlDelay(delayTime).then((resolve, reject) => {
+        try {
+            const response = await fetch(url);
+            resolve(response);
+        } catch (e) { reject(e); }
+
+    });
+    return {
+        result: response,
+        timestamp: Date.now()
+    }
+}
+
 exports.fetchRobots = async (baseUrl) => {
     const response = await fetch(new URL(ROBOTS_PATH, baseUrl));
     const robotsText = await response.text();
