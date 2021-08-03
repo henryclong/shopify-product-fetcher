175ef0c13e8eae7f86799d09edf0e619049d8283
Henry Long <henry.cassetta.long@gmail.com>
2021-08-03 15:10:28 -0400

    Ensure that fetch calls respect crawl delay. If crawl delay not found in robots.txt, falls back to default value

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const { fetchRobots, fetchXML } = require('./fetch');
 const { getProductSitemaps, getProductUrls } = require('./parseSitemap');
+const defaults = require('../config/defaults.json');
 
 const USER_AGENT = '*';
 
 const fetchAllProductURLs = async (baseUrl) => {
     const { robots, timestamp: robotsTimestamp } = await fetchRobots(baseUrl);
     const sitemapUrl = robots[USER_AGENT].Sitemap;
-    const { sitemap: sitemap, timestamp: sitemapTimestamp } = await fetchXML(sitemapUrl);
+    const crawlDelay = robots[USER_AGENT]['Crawl-delay'] ? robots[USER_AGENT]['Crawl-delay'] : defaults.crawlDelay;
+    const { sitemap: sitemap, timestamp: sitemapTimestamp } = await fetchXML(sitemapUrl, crawlDelay, robotsTimestamp);
     const productSitemapUrls = getProductSitemaps(sitemap);
     const productUrls = [];
+    let productSitemapTimestamp;
     for (productSitemapUrl of productSitemapUrls) {
-        const { sitemap: productSitemap, timestamp: productSitemapTimestamp } = await fetchXML(productSitemapUrl);
+        const { sitemap: productSitemap, timestamp: latestProductSitemapTimestamp } = await fetchXML(productSitemapUrl, crawlDelay, productSitemapTimestamp ? productSitemapTimestamp : sitemapTimestamp);
+        productSitemapTimestamp = latestProductSitemapTimestamp;
         productUrls.push(...getProductUrls(productSitemap));
     }
     return productUrls;
