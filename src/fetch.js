dbf13c4d34d5552258d4a960e09c6024f2ce7b22
Henry Long <henry.cassetta.long@gmail.com>
2021-08-03 15:34:31 -0400

    Document index and fetch

--- src/fetch.js
+++ src/fetch.js
@@ -5,16 +5,31 @@ const defaults = require('../config/defaults.json');
 
 const ROBOTS_PATH = '/robots.txt';
 
+/**
+ * Pause execution of code for a specified amount of time
+ * @param {number} ms - The number of milliseconds to pause
+ */
 const crawlDelay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Calculate the amount of time to wait in order to respect the specified Crawl-delay
+ * @param {number} crawlDelay - The amount of time to wait between requests
+ * @param {number} lastVisitTimestamp - The timestamp of the latest request
+ */
 const getCrawlDelay = (crawlDelay, lastVisitTimestamp) => {
     const currentTimestamp = Date.now();
     let delay = (lastVisitTimestamp + crawlDelay) - currentTimestamp;
     return delay > 0 ? delay : 0;
 }
 
+/**
+ * Fetch data from a specified url, waiting long enough to respect the crawl-delay
+ * @param {string} url - Location of the fetched resource
+ * @param {number} delay - The amount of time to wait between requests
+ * @param {number} lastVisitTimestamp - The timestamp of the latest request
+ */
 const fetchWithCrawlDelay = async (url, delay, lastVisitTimestamp) => {
     const delayTime = getCrawlDelay(delay ? delay : defaults.crawlDelay, lastVisitTimestamp);
     await crawlDelay(delayTime);
@@ -28,14 +43,26 @@ const fetchWithCrawlDelay = async (url, delay, lastVisitTimestamp) => {
     } catch (e) { console.error(e); }
 }
 
+/**
+ * Fetch data from a specified url, waiting long enough to respect the crawl-delay
+ * @param {string} baseUrl - Base url to fetch robots.txt from
+ * @param {number} delay - The amount of time to wait between requests
+ * @param {number} lastVisitTimestamp - The timestamp of the latest request
+ */
 exports.fetchRobots = async (baseUrl, delay, lastVisitTimestamp) => {
     const { textContent: robotsText, timestamp } = await fetchWithCrawlDelay(new URL(ROBOTS_PATH, baseUrl), delay, lastVisitTimestamp);
     const robots = parseRobots(robotsText);
     return { robots: robots, timestamp: timestamp };
 }
 
-exports.fetchXML = async (sitemapUrl, delay, lastVisitTimestamp) => {
-    const { textContent: sitemapText, timestamp } = await fetchWithCrawlDelay(sitemapUrl, delay, lastVisitTimestamp);
-    const sitemap = await parseXML(sitemapText);
-    return { sitemap: sitemap, timestamp: timestamp };
+/**
+ * Fetch data from a specified url, waiting long enough to respect the crawl-delay
+ * @param {string} xmlUrl - Location of fetched xml file
+ * @param {number} delay - The amount of time to wait between requests
+ * @param {number} lastVisitTimestamp - The timestamp of the latest request
+ */
+exports.fetchXML = async (xmlUrl, delay, lastVisitTimestamp) => {
+    const { textContent: xmlText, timestamp } = await fetchWithCrawlDelay(xmlUrl, delay, lastVisitTimestamp);
+    const xml = await parseXML(xmlText);
+    return { xml: xml, timestamp: timestamp };
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,27 @@ const defaults = require('../config/defaults.json');
 
 const USER_AGENT = '*';
 
+/**
+ * Fetches all product urls for a specified Shopify store
+ * @param {string} baseUrl - The URL of the store to fetch from
+ */
 const fetchAllProductURLs = async (baseUrl) => {
+    // Fetch robots.txt from the store, and save the parent sitemap url & crawl delay
     const { robots, timestamp: robotsTimestamp } = await fetchRobots(baseUrl);
     const sitemapUrl = robots[USER_AGENT].Sitemap;
     const crawlDelay = robots[USER_AGENT]['Crawl-delay'] ? robots[USER_AGENT]['Crawl-delay'] : defaults.crawlDelay;
-    const { sitemap: sitemap, timestamp: sitemapTimestamp } = await fetchXML(sitemapUrl, crawlDelay, robotsTimestamp);
+    
+    // Fetch the parent sitemap content from the store using the URL provided by robots.txt
+    const { xml: sitemap, timestamp: sitemapTimestamp } = await fetchXML(sitemapUrl, crawlDelay, robotsTimestamp);
     const productSitemapUrls = getProductSitemaps(sitemap);
+
+    // Iterate through the product sitemap urls, and extract the product urls from their contents
     const productUrls = [];
     let productSitemapTimestamp;
     for (productSitemapUrl of productSitemapUrls) {
-        const { sitemap: productSitemap, timestamp: latestProductSitemapTimestamp } = await fetchXML(productSitemapUrl, crawlDelay, productSitemapTimestamp ? productSitemapTimestamp : sitemapTimestamp);
+
+        // Fetch the product sitemap content, and save the product urls stored within
+        const { xml: productSitemap, timestamp: latestProductSitemapTimestamp } = await fetchXML(productSitemapUrl, crawlDelay, productSitemapTimestamp ? productSitemapTimestamp : sitemapTimestamp);
         productSitemapTimestamp = latestProductSitemapTimestamp;
         productUrls.push(...getProductUrls(productSitemap));
     }
